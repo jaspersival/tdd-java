f96c6723a9e965cb247305aebbe3e70506ddfca8
Jasper Sival <jasper.sival@alliander.com>
2025-08-17 13:42:56 +0200

    MultipleOfThree now works for 3 as input

--- src/main/java/alliander/jsival/FizzBuzzer.java
+++ src/main/java/alliander/jsival/FizzBuzzer.java
@@ -2,6 +2,10 @@ package alliander.jsival;
 
 public class FizzBuzzer {
     public String fizzBuzz(Number num) {
+        if (num.intValue() == 3) {
+            return "Fizz";
+        }
         return num.toString();
     }
+
 }
--- src/test/java/TestFizzBuzz.java
+++ src/test/java/TestFizzBuzz.java
@@ -1,12 +1,24 @@
 import alliander.jsival.FizzBuzzer;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TestFizzBuzz {
+    private FizzBuzzer fizzBuzzer;
+
+    @BeforeEach
+    void setUp() {
+        fizzBuzzer = new FizzBuzzer();
+    }
+
     @Test
     void testShouldReturnStringGivenNumber() {
-        FizzBuzzer fizzBuzzer = new FizzBuzzer();
         String result = fizzBuzzer.fizzBuzz(1);
         assertEquals("1", result);
+    }
+    @Test
+    void testShouldReturnFizzGivenMultipleOfThree() {
+        String result = fizzBuzzer.fizzBuzz(3);
+        assertEquals("Fizz", result);
     }
 }
